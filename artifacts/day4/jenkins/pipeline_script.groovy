ec68b17986597573817d4f16d270be5e030d4e6c
Мирас <miras@student.local>
2026-03-26 19:22:40 +0500

    Complete Day 4 Jenkins pipeline

--- artifacts/day4/jenkins/pipeline_script.groovy
+++ artifacts/day4/jenkins/pipeline_script.groovy
@@ -1,1 +1,23 @@
-pending
+pipeline {
+    agent any
+
+    stages {
+        stage('Preparation') {
+            steps {
+                echo 'Preparation stage'
+            }
+        }
+
+        stage('Build') {
+            steps {
+                echo 'Build stage'
+            }
+        }
+
+        stage('Results') {
+            steps {
+                echo 'Results stage'
+            }
+        }
+    }
+}
